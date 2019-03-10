e47990485e706b708a755cf57e098a1661b6ea13
Pramod KS <40273420+PriyaPramod@users.noreply.github.com>
2019-03-10 10:24:03 +0530

    Updating the Grid

--- source/utilities/webdriver_manager.py
+++ source/utilities/webdriver_manager.py
@@ -5,10 +5,17 @@ from source.utilities import helpers
 
 def create_driver(browser_name, url):
     driver = None
+    hub_url = "http://192.168.88.2:4444/wd/hub"
+    chrome_capability = {"platform":"windows",
+                         "browserName":"chrome"}
+    
+    ff_capability = {"platform":"windows",
+                         "browserName":"firefox"}
+    
     if browser_name == "Chrome":
-        driver = webdriver.Chrome(executable_path=const.CHROME_VALUE)
+        driver = webdriver.Remote(command_executor=hub_url, desired_capabilities=chrome_capability)
     elif browser_name == "ff":
-        driver = webdriver.Firefox(executable_path=const.FF_VALUE)
+        driver = webdriver.Remote(command_executor=hub_url, desired_capabilities=ff_capability)
     
     driver.maximize_window()
     driver.get(url)
